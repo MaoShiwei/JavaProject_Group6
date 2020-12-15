d0b5b89e87639a870e3db9a922891ae13f1216e9
Elbert <19262724@qq.com>
2020-12-16 01:57:25 +0800

    提交作业三

---
+++ Blob.java
@@ -0,0 +1,30 @@
+package gitsimulator;
+
+import java.io.*;
+
+public class Blob extends KeyValueStorage {
+	private String type = "blob";
+	private String path;
+	private String key;	
+	private File file;
+	private String vrtype = "100644";
+	
+    public Blob(String path) throws Exception {
+    	this.file = new File(path);
+        this.key = GetKey(new FileInputStream(file));
+        this.path = path;
+    } 
+    
+    public void write() throws Exception {
+        WriteToFile(path);
+    }
+    
+	public String GetKey() {
+		return key;
+	}
+    
+    @Override
+    public String toString() {
+        return vrtype + " " + type + " " + key;
+    }
+}
