c67df54ecb5ef5dd82964e309675d6805540da9f
Jericaran <70431320+Jericaran@users.noreply.github.com>
2021-01-05 19:02:29 +0800

    update keyvaluetest in unitTest

--- 交互式测试/unitTest.java
+++ 交互式测试/unitTest.java
@@ -1,6 +1,7 @@
 package gitsimulator;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class unitTest {
@@ -36,14 +37,16 @@ public class unitTest {
         System.out.println(commit.ShowCommitList());
     }
 
+
+
     //生成Tree测试
     public static void testTree(File file){
         try{
             if(file.isDirectory()){
-                System.out.println(new Tree(file).GetKey());
+                System.out.println("Tree:"+new Tree(file).GetKey());
             }
             else{
-                System.out.println(new Blob(file).GetKey());
+                System.out.println("Blob:"+new Blob(file).GetKey());
             }
         }
         catch (Exception ex){
@@ -51,14 +54,34 @@ public class unitTest {
         }
     }
 
+    //生成value测试
+    public static void testKeyValueStorage(File file) throws Exception{
+        try{
+            if(file.isFile()) {
+                KeyValueStorage keyValueStorage = new KeyValueStorage();
+                //获取文件key
+                String key = keyValueStorage.GetKey(new FileInputStream(file));
+                //将文件重新写入 用于下面GetValue 获取到文件
+                keyValueStorage.WriteToFile(keyValueStorage.GetValue(key), "D:\\Java\\managebase\\content.txt");
+                //通过key获取文件
+                String s = keyValueStorage.GetValue(key);
+                System.out.println("文件的内容value是：" + s);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public static void main(String args[]) throws Exception {
         Scanner input = new Scanner(System.in);
         System.out.println("请输入文件或文件夹的路径名：");
         String path = input.next();
         testSingleKey(path);
-        testCommit(path);
-        testCommit("D:\\Java\\homework1105");
+        //testCommit(path);
+        //testCommit("D:\\Java\\homework1105");
         testTree(new File(path));
+        testKeyValueStorage(new File(path));
         HEAD head = new HEAD();
         System.out.println(head.getHEAD());
     }
